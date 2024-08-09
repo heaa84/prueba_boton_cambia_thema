8426f89334b48324c31ea7bc8a41dcf6c110b95d
heaa84 <hugo.alarcon@icorp.com.mx>
2024-08-09 06:22:04 -0600

    vercion final

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,71 +1,47 @@
 const btnTheme=document.querySelector(".borde__btn__theme");
-const esfera=document.querySelector("#btnTheme");
+const esfera=document.querySelector("#esfera");
 const body=document.querySelector("body");
-const texto=document.querySelector("body");
-let posicionEsfera=0;
-//colores en tema claro
-let colorFondoBody=`hwb(180 20% 50% / 0.3)`;
-let colorFondoEsfera=`hwb(200 40% 10% / 0.3)`;
-let colorBorderEsfera=`hwb(180 40% 30% / 0.3)`;
-let colorFondoBoton=`hwb(180 20% 60% / 0.3)`;
-let colorBorderBoton=`hwb(180 20% 50% / 0.3)`;
-let colortextoBody=`hwb(180 50% 70%)`;
+let posicionEsfera=1;
 
-//colores en tema oscuro
-let colorFondoBodyOscuro=`hwb(180 10% 75%)`;
-let colortextoBodyOscuro=`hwb(180 30% 25%)`;
-let colorBorderBotonOscuro=`hwb(180 50% 20%)`;
-let colorFondoEsferaOscuro=`hwb(200 20% 20%)`;
-let colorBorderEsferaOscuro=`hwb(180 40% 30%)`;
-
-//clase de los elementos
-class elementos {
-    constructor(elemento, hwbBackground, hwbBorder, hwbTexto ,posicionEsfera) {
-        this.elemento=elemento;
-        this.hwbBackground=hwbBackground;
-        this.hwbBorder=hwbBorder;
-        this.hwbTexto=hwbTexto;
-        this.posicionEsfera=posicionEsfera;
+class btn {
+    cambiarTema=()=>{
+        if (posicionEsfera==1) {// si la esfera esta ala izquierda
+            var intervaloTiempo=setInterval(moverDerecha, 5);
+            var blanco=0;
+            var tonoBtn=100;
+            var tonobody=100; 
+            function moverDerecha() {
+                posicionEsfera++;
+                esfera.style.left=`${posicionEsfera}px`;  
+                esfera.style.background=`radial-gradient(circle at bottom,hwb(0 100% 15%),hwb(112 ${blanco+=3.3}% 0%))`;
+                btnTheme.style.backgroundColor= `hwb(112 ${tonoBtn-=3.448275862068966}% 3%)`;
+                body.style.background=`hwb(200 ${tonobody-=3.448275862068966}% 10%)`;
+                if (posicionEsfera==30) {
+                    clearInterval(intervaloTiempo);
+                }
+            };
+        }else{
+            if (posicionEsfera==30) {// is la esfera esta a la derecha
+                var blanco=100;
+                var tonoBtn=0;
+                var tonobody=0;
+                var intervaloTiempo=setInterval(moverIzquierda, 5);
+                function moverIzquierda() {
+                    posicionEsfera--;
+                    esfera.style.left=`${posicionEsfera}px`;
+                    esfera.style.background=`radial-gradient(circle at bottom,hwb(122 1% 5% / 0.8),hwb(112 ${blanco-=3.448275862068966}% 3%))`;
+                    btnTheme.style.backgroundColor= `hwb(112 ${tonoBtn+=3.379310344827586}% 2%)`                    
+                    body.style.background=`hwb(200 ${tonobody+=2.758620689655172}% 10%)`;//background-color: hwb(200 80% 10%);
+                    if (posicionEsfera==1) {
+                        clearInterval(intervaloTiempo);
+                    }
+                };
+            }
+        }
     }
-    coloriarElemento = () => {        
-        this.elemento.style.background=`${this.hwbBackground}`;
-        this.elemento.style.borderColor=`${this.hwbBorder}`;    
-        this.elemento.style.color=`${this.hwbTexto}`; 
-        this.elemento.style.left=`${this.posicionEsfera}px`;
-    }
-};
-//instancias de objetos elementos en modo claro
-const MyBody= new elementos(body, colorFondoBody , null , colortextoBody );
-const MyEsfera= new elementos(esfera, colorFondoEsfera,colorBorderEsfera,null,0);
-const MyBtnTheme= new elementos(btnTheme, colorFondoBoton,colorBorderBoton);
+}
 
-//modo tema oscuro
-const MyBodyOscuro= new elementos(body, colorFondoBodyOscuro , null ,colortextoBodyOscuro );
-const MyBtnThemeOscuro= new elementos(btnTheme, colorFondoBoton,colorBorderBotonOscuro);
-const MyEsferaOscura= new elementos(esfera, colorFondoEsferaOscuro,colorBorderEsferaOscuro,null,34);
-
-//invocando metodos tema claro
-MyBody.coloriarElemento();
-MyEsfera.coloriarElemento();
-MyBtnTheme.coloriarElemento();
-
-// cuando se de click al boton
+const cambiar__tema= new btn();
 btnTheme.addEventListener("click",()=>{
-    //lanza modo oscuro si la esfera esta a la izquierda
-    if(posicionEsfera==0){
-        MyBodyOscuro.coloriarElemento();
-        MyBtnThemeOscuro.coloriarElemento();
-        MyEsferaOscura.coloriarElemento();
-        posicionEsfera=34;
-    }else{
-        //lanza modo claro si la esfera esta a la derecha
-        if(posicionEsfera==34){
-            MyBody.coloriarElemento();
-            MyEsfera.coloriarElemento();
-            MyBtnTheme.coloriarElemento();
-            posicionEsfera=0;
-            console.log(posicionEsfera);
-            
-        };
-    }
-})
+    cambiar__tema.cambiarTema();
+});
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Boton cambia color de tema
+
+Este proyecto es un ejemplo de un botón que permite cambiar el tema de color de una página web. Utiliza la clase btn para el estilo del botón e instancia el botón con el nombre cambiar__tema. El cambio de color se realiza utilizando el modelo de color HWB (hwb(tono, blanco, negro)), lo que facilita la personalización del tema. Además, el botón incluye una pequeña animación de movimiento cada vez que se le da clic.
+
+### caracteristicas:
+* **Cambio de tema:** El botón cambia el tema de color de la página web utilizando el modelo de color HWB.
+
+* **Animacion:** Cada vez que se hace click en el boton, se activa una animacio de movimiento.
+
+* **Fácil Personalización:** Cambiando el valor del HWB, podemos cambiar el color del tema.
+
+## Licencia: de licencia libre, sientete libre de hacer lo que quiereas con el.
